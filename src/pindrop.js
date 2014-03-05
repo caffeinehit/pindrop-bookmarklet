73ace50cde5e098b4715c7098e14edd5f66dd86d
Tom Drummond <tom@devioustree.co.uk>
2014-03-05 15:25:41 +0000

    Added some functionality

--- src/pindrop.js
+++ src/pindrop.js
@@ -1,41 +1,52 @@
 (function() {
     var PinDrop = window.PinDrop || {};
+    window.pindrop_cfg = {};
+    window.pindrop_cfg.debug = true;
 
     if (window.JSON === undefined || typeof window.JSON != 'object') {
         alert("We're sorry, our bookmarklet works only on recent browsers. Please update your browser and try again.");
         return;
     }
 
-    var logger = function() {
-        if (window.dcovery_cfg.debug === true) {
+    var logger = function(message) {
+        if (window.pindrop_cfg.debug === true) {
             if (typeof window.console === 'object' && typeof window.console.log === 'function') {
-                console.log(Array.prototype.slice.call(arguments));
+                console.log(message);
             }
         }
     };
-    
+
     if (typeof PinDrop.start === 'function') {
         logger("pindrop> Pin Drop is already loaded.");
         PinDrop.start(false);
         return;
     }
 
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.onload = startFindingAddresses;
-    script.src = '//raw.github.com/sheriff/ziprip/master/dist/ziprip-latest.js';
-    head.appendChild(script);
+    PinDrop.start = function(init) {
+        PinDrop.insertAddressFinder();
+    }
 
-    var startFindingAddresses = function() {
-        var addresses = window.ziprip.extract( document, window.URL );
-        for (var i = addresses.length - 1; i >= 0; i--) {
-            var address = addresses[i];
-            console.log(address.formatForGeocode());
-        }
+    PinDrop.insertAddressFinder = function() {
+        var startFindingAddresses = function() {
+            logger("Finding addresses");
+            var addresses = window.ziprip.extract(document, window.URL);
+            for (var i = addresses.length - 1; i >= 0; i--) {
+                var address = addresses[i];
+                logger(address.formatForGeocode());
+            }
 
-        if (addresses.length === 0) {
-            console.log("No addresses found");
-        }
-    };
+            if (addresses.length === 0) {
+                logger("No addresses found");
+            }
+        };
+
+        var head = document.getElementsByTagName('head')[0];
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.onload = startFindingAddresses;
+        script.src = '//raw.github.com/sheriff/ziprip/master/dist/ziprip-latest.js';
+        head.appendChild(script);
+    }
+
+    PinDrop.start(true);
 })();
